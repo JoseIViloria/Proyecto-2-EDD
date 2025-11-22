222b6c8be3c99fc58afc7ab3ac82d0654ed7056e
LuisEVW <luisviloria@correo.unimet.edu.ve>
2025-11-22 04:15:25 -0400

    Implementación de Árbol AVL

---
+++ Proyecto2EDD/src/proyecto2edd/Arbol.java
@@ -0,0 +1,125 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2edd;
+import java.util.Locale;
+import java.text.Collator;
+/**
+ *
+ * @author 
+ */
+public class Arbol {
+    private Nodo root;
+    
+    public Arbol() {
+        this.root = null;
+    }
+    
+    public boolean esVacio(Nodo tRoot){
+        return tRoot==null;
+    }
+    
+    public Nodo getroot(){
+        return this.root;
+    }
+    
+    public void setroot(Nodo n){
+        this.root=n;
+    }
+    
+    public int máximo(int a, int b){
+        if(a>b){
+            return a;
+        }
+        else{
+            return b;
+        }
+    }
+    
+    public int altura(Nodo n){
+        if (n==null){
+            return 0;
+        }
+        return n.getAltura();
+    }
+    
+    public int balance(Nodo n){
+        
+        if (n==null){
+            return 0;
+        }
+        return altura(n.getHijo_der()) - altura(n.getHijo_izq());
+    }
+    
+    public Nodo insertar(Nodo raíz, String autor){
+        Collator collator = Collator.getInstance(new Locale("es", "ES"));
+        if (this.root==null){
+            this.setroot(new Nodo(autor));
+        }
+        if (esVacio(raíz)){
+            return new Nodo(autor);
+        }
+        else if(collator.compare(autor, raíz.getautor())<0){
+            raíz.setHijo_izq(insertar(raíz.getHijo_izq(), autor));
+        }
+        else if(collator.compare(autor,raíz.getautor())>0){
+            raíz.setHijo_der(insertar(raíz.getHijo_der(), autor));
+        }
+        return raíz;
+    }
+    
+    public void actualizaraltura(Nodo n){
+        n.setAltura(máximo(altura(n.getHijo_izq()),altura(n.getHijo_der())));
+    }
+    
+    public Nodo rebalancear(Nodo raíz){
+        int balance = balance(raíz);
+        if(balance<-1){
+            if(balance(raíz.getHijo_izq())<=0){
+                raíz = rotarderecha(raíz);
+            }
+            else{
+                raíz.setHijo_izq(rotarizquierda(raíz.getHijo_izq()));
+                raíz=rotarderecha(raíz);
+            }
+        }
+        if(balance>1){
+            if(balance(raíz.getHijo_der())>=0){
+                raíz = rotarizquierda(raíz);
+            }
+            else{
+                raíz.setHijo_der(rotarderecha(raíz.getHijo_der()));
+                raíz = rotarizquierda(raíz);
+            }
+        }
+        return raíz;
+    }
+    
+    public Nodo rotarderecha(Nodo n){
+        Nodo izq=n.getHijo_izq();
+        n.setHijo_izq(izq.getHijo_der());
+        izq.setHijo_der(n);
+        actualizaraltura(n);
+        actualizaraltura(izq);
+        return izq;
+    }
+    
+    public Nodo rotarizquierda(Nodo n){
+        Nodo der = n.getHijo_der();
+        n.setHijo_der(der.getHijo_izq());
+        der.setHijo_izq(n);
+        actualizaraltura(n);
+        actualizaraltura(der);
+        return der;
+    }
+    
+    public Nodo InsertarNodo(Nodo raíz, String AUTOR){
+        raíz = insertar(raíz, AUTOR);
+        actualizaraltura(raíz);
+        return rebalancear(raíz);
+    }
+    
+    
+
+}
---
+++ Proyecto2EDD/src/proyecto2edd/Nodo.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2edd;
+
+/**
+ *
+ * @author 
+ */
+public class Nodo {
+    private Nodo hijo_der;
+    private Nodo hijo_izq;
+    private String autor;
+    private int altura;
+
+    public Nodo(String autor) {
+        this.hijo_der = null;
+        this.hijo_izq = null;
+        this.autor = autor;
+        this.altura = 1;
+    }
+    
+    /**
+     * 
+     * @return altura
+     */
+    public int getAltura() {
+        return altura;
+    }
+    
+    /**
+     * 
+     * @param altura 
+     */
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+    
+
+    /**
+     * @return the hijo_der
+     */
+    public Nodo getHijo_der() {
+        return hijo_der;
+    }
+
+    /**
+     * @param hijo_der the hijo_der to set
+     */
+    public void setHijo_der(Nodo hijo_der) {
+        this.hijo_der = hijo_der;
+    }
+
+    /**
+     * @return the hijo_izq
+     */
+    public Nodo getHijo_izq() {
+        return hijo_izq;
+    }
+
+    /**
+     * @param hijo_izq the hijo_izq to set
+     */
+    public void setHijo_izq(Nodo hijo_izq) {
+        this.hijo_izq = hijo_izq;
+    }
+
+    /**
+     * @return the autor
+     */
+    public String getautor() {
+        return autor;
+    }
+
+    /**
+     * @param autor the autor to set
+     */
+    public void setautor(String autor) {
+        this.autor = autor;
+    }
+    
+}
